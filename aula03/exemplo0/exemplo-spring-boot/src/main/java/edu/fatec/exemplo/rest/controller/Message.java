58a356df1cfd43acd4cda6ed3d7a35c477191fa9
Cursos Laboratório de T.I <cursos@lti.fatecso.edu.br>
2016-05-07 12:51:28 -0300

    Spring Boot modifications 0705

---
+++ aula03/exemplo0/exemplo-spring-boot/src/main/java/edu/fatec/exemplo/rest/controller/Message.java
@@ -0,0 +1,34 @@
+package edu.fatec.exemplo.rest.controller;
+
+public class Message {
+	private int code;
+	private String message;
+	
+	public Message() {
+	
+	}
+	
+
+	public Message(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return "Message [code=" + code + ", message=" + message + "]";
+	}
+}
--- aula03/exemplo0/exemplo-spring-boot/src/main/java/edu/fatec/exemplo/rest/controller/MoviesController.java
+++ aula03/exemplo0/exemplo-spring-boot/src/main/java/edu/fatec/exemplo/rest/controller/MoviesController.java
@@ -1,10 +1,34 @@
 package edu.fatec.exemplo.rest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/movie")
 public class MoviesController {
+	
+	@RequestMapping("")
+	@ResponseBody
+	public String helloWorld() {
+		return "Hello World";
+	}
+	@RequestMapping (value = "/message", 
+					method = RequestMethod.GET,
+					produces = "application/json")
+	public @ResponseBody Message getMessage() {
+		return new Message(200, "HelloWorld");
+	}
+	
+	@RequestMapping (value = "/message", 
+			method = RequestMethod.POST,
+			produces = "application/json",
+			consumes = "application/json")
+	public @ResponseBody Message printMessage(@RequestBody Message message) {
+		System.out.println(message);
+		return message;
+	}
 
 }
